e8dedfd6fa053a4dc9d2b061d357040baa0e01f2
stefanwerleman <stefanwerleman@knights.ucf.edu>
2022-02-26 15:33:54 -0500

    Removed unecessary code

--- src/main_sim.cpp
+++ src/main_sim.cpp
@@ -1,7 +1,6 @@
 #include "../libs/ArgumentWrapper/ArgumentWrapper.h"
 #include "../libs/Cache/Cache.h"
 #include <iostream>
-#include <fstream>
 #include <string>
 #include "../libs/utils/utils.h"
 
@@ -22,22 +21,8 @@ int main(int argc, char **argv)
 
     std::vector<Cache*> memory_hierarchy = utils::create_memory_hierarchy(arguments);
 
-    std::ifstream file("./data/traces/" + arguments.get_trace_file());
-
-    std::string in;
-
-    while (file >> in)
-    {
-        char operation = in[0];
-        file >> in;
-        std::string address = in;
-
-        std::cout << memory_hierarchy[0]->parse_address(operation, address, arguments.get_block_size());
-    }
-
-    // TODO: Begin Simulation
+    // TODO: Begin Simulation after you understand replacement policies
     
-    file.close();
     utils::destroy_memory_hierarchy(memory_hierarchy);
 	return 0;
 }
